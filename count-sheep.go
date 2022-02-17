50340bec207fe1c7fb1ebe12df78a1b5c9a15bef
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-17 14:06:48 +0300

    I added solution explanation

--- count-sheep.go
+++ count-sheep.go
@@ -1,3 +1,14 @@
+/*
+
+Solution Explanation
+
+- First I returned its value based on the condition of being 0.
+- I made a string definition called "temp" to keep the repeat count.
+- I found how many times it was written with a for loop.
+- I converted every value I found to integer to string, then returned the result.
+
+*/
+
 package kata
 
 import(
