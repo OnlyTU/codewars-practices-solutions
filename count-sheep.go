3fa74b40355785c1f25d7ce390c8d7f3f68b8772
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-17 13:50:32 +0300

    8kyu-If you can't sleep, just count sheep!!

---
+++ count-sheep.go
@@ -0,0 +1,19 @@
+package kata
+
+import(
+  "strconv"
+)
+
+func countSheep(num int) string {
+  
+  if num == 0 {
+    return ""
+  }
+  
+  temp := ""
+  for i := 1; num >= i; i++ {    
+      s := strconv.Itoa(i)
+      temp += s + " sheep..."
+    }
+  return temp 
+}
