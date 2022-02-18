70b369e80c612af0eaf6a57fda4c583c97358f76
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-18 11:06:09 +0300

    8kyu-Is he gonna survive?

---
+++ hero-survive.go
@@ -0,0 +1,17 @@
+   
+/*
+
+Solution Explanation
+
+- Every dragons take 2 bullets that's why 2 times the number of dragons must be equal to or less than the number of bullets.
+
+*/
+
+package kata
+
+func Hero(bullets, dragons int) bool {
+  if (dragons * 2 <= bullets ) {
+    return true
+  }
+    return false 
+}
