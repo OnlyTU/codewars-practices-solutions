567c1be7167614abce9cea770ed338a5249e4eb4
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 00:00:30 +0300

    8kyu - Get Nth Even Number

---
+++ which-even-number.go
@@ -0,0 +1,14 @@
+/*
+
+Solution Explanation
+
+- If we multiply the given number by 2 and subtract 2, we can find which even number it corresponds to. 
+
+*/
+
+
+package kata
+
+func NthEven(n int) int {
+  return 2 * n - 2
+}
