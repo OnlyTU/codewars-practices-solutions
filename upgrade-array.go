6e10a0bd1acb76030535dba80d8f2a6d9397e47e
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:41:24 +0300

    8kyu -  Beginner - Lost Without a Map

--- upgrade-array.go
+++ upgrade-array.go
@@ -1,6 +1,15 @@
 /*
 
-Solution Explanation
+Question:
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+Solution Explanation:
 
 - Multiplying each element of the given array by 2 and creating a new array without breaking the order.
 
@@ -17,3 +26,17 @@ func Maps(x []int) []int {
   }
   return s
 }
+
+/*
+
+Test:
+
+var _ = Describe("Example tests", func() {
+  It("should test that the solution returns the correct value", func() {
+    Expect(Maps([]int{1, 2, 3})).To(Equal([]int{2, 4, 6}))
+    Expect(Maps([]int{4, 1, 1, 1, 4})).To(Equal([]int{8, 2, 2, 2, 8}))
+    Expect(Maps([]int{2, 2, 2, 2, 2, 2})).To(Equal([]int{4, 4, 4, 4, 4, 4}))
+  })
+})
+
+*/
