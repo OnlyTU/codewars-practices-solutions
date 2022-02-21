b76c159a65db0045c144030676f94bba5afbd6a0
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:35:18 +0300

    8kyu - Find the smallest integer in the array

--- smallest-element.go
+++ smallest-element.go
@@ -1,6 +1,16 @@
 /*
 
-Solution Explanation
+Question:
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+Solution Explanation:
 
 - We can assume the first element of the given array as the smallest and compare it with the other elements to find the smallest number.
 
@@ -20,3 +30,16 @@ func SmallestIntegerFinder(numbers []int) int {
   }
   return min 
 }
+
+/*
+
+Test:
+
+var _ = Describe("Sample Test Cases", func() {
+  It("should work for sample tests", func() {
+    Expect(Expect(SmallestIntegerFinder([]int{34, 15, 88, 2})).To(Equal(2)))
+    Expect(Expect(SmallestIntegerFinder([]int{34, -345, -1, 100})).To(Equal(-345)))
+  })
+})
+
+*/
