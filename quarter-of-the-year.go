35e78fc7fb10319061fe91ce603ed3df54cd6bed
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 16:14:58 +0300

    8kyu - Quarter of the year(Small update)

--- quarter-of-the-year.go
+++ quarter-of-the-year.go
@@ -17,10 +17,11 @@ For example: month 2 (February), is part of the first quarter; month 6 (June), i
 package kata
 
 func QuarterOf(month int) int {
-  if month > 9 && month < 13{ return 4 }
-  if month > 6 && month < 10{ return 3 }
-  if month > 3 && month < 7{ return 2 }
-  return 1
+  if month > 9 && month < 13 { return 4 }
+  if month > 6 && month < 10 { return 3 }
+  if month > 3 && month < 7 { return 2 }
+  if month > 0 && month < 4 { return 1 }
+  return 0
 }
 
 /*
