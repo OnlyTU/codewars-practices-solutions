dc2337cc840446b92d5cb2245cd95d7c48530898
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:21:59 +0300

    8kyu - Is he gonna survive?

--- hero-survive.go
+++ hero-survive.go
@@ -1,7 +1,13 @@
    
 /*
 
-Solution Explanation
+- Question
+
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+
+- Solution Explanation
 
 - Every dragons take 2 bullets that's why 2 times the number of dragons must be equal to or less than the number of bullets.
 
@@ -11,19 +17,23 @@ Solution Explanation
 package kata
 
 func Hero(bullets, dragons int) bool {
-  if (dragons * 2 <= bullets ) {
-    return true
-  }
-    return false 
+  return dragons * 2 <= bullets
 }
 
-
-// Single line solution
 
 /*
 
-func Hero(bullets, dragons int) bool {
-  return dragons * 2 <= bullets
-}
+- Test
+
+var _ = Describe("Fixed tests", func() {
+   It("should test that the solution returns the correct value", func() {
+     Expect(Hero(10, 5)).To(Equal(true))
+     Expect(Hero(7, 4)).To(Equal(false))
+     Expect(Hero(4, 5)).To(Equal(false))
+     Expect(Hero(100, 40)).To(Equal(true))
+     Expect(Hero(1500, 751)).To(Equal(false))
+     Expect(Hero(0, 1)).To(Equal(false))
+   })
+})
 
 */
