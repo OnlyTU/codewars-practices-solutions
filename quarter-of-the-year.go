21342a1d312e06594e48fec6b2302bfc776200be
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:14:51 +0300

    8kyu - Quarter of the year (Updated)

--- quarter-of-the-year.go
+++ quarter-of-the-year.go
@@ -1,6 +1,13 @@
 /*
 
-Solution Explanation
+- Question
+
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+
+- Solution Explanation
 
 - In order to calculate which quarter the given month belongs to, it will be sufficient to find between which months that month belongs.
 
@@ -15,3 +22,18 @@ func QuarterOf(month int) int {
   if month > 3 && month < 7{ return 2 }
   return 1
 }
+
+/*
+
+- Test
+
+var _ = Describe("Test Example", func() {
+  It("should test example values", func() {
+    Expect(QuarterOf(3)).To(Equal(1))
+    Expect(QuarterOf(8)).To(Equal(3))
+    Expect(QuarterOf(11)).To(Equal(4))
+  })
+})
+
+
+*/
