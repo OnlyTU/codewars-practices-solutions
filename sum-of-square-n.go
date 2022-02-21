87f4fb64488e666b13b9d7498dc9c1ad6f802129
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:37:42 +0300

    8kyu - Square(n) Sum

--- sum-of-square-n.go
+++ sum-of-square-n.go
@@ -1,6 +1,13 @@
  /*
  
-Solution Explanation
+Question:
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+ 
+Solution Explanation:
 
 - If we multiply each element of the given array by itself and add up the obtained values, we can find the expected answer.
 
@@ -21,3 +28,15 @@ func SquareSum(numbers []int) int {
   }  
   return 0
 }
+
+ /*
+ 
+Test:
+
+var _ = Describe("Sample Tests",func() {
+    It("Testing [1,2]",func() {Expect(SquareSum([]int{1,2})).To(Equal(5))})
+    It("Testing [0,3,4,5]",func() {Expect(SquareSum([]int{0,3,4,5})).To(Equal(50))})
+    It("Testing []",func() {Expect(SquareSum([]int{})).To(Equal(0))})
+})
+
+*/
