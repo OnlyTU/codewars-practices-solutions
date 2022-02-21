e84ca8c9945e8e7f6709048f5ec5edfd0a45c67e
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:24:59 +0300

    8kyu - If you can't sleep, just count sheep!!

--- count-sheep.go
+++ count-sheep.go
@@ -1,6 +1,10 @@
 /*
 
-Solution Explanation
+Question:
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+Solution Explanation:
 
 - First I returned its value based on the condition of being 0.
 - I made a string definition called "temp" to keep the repeat count.
@@ -28,3 +32,17 @@ func countSheep(num int) string {
     }
   return temp 
 }
+
+/*
+
+Test:
+
+var _ = Describe("Sample Test Cases", func() {
+	It("The solution should return the correct values for the sample test cases!", func() {
+		Expect(countSheep(2)).To(Equal("1 sheep...2 sheep..."))
+		Expect(countSheep(0)).To(Equal(""))
+		Expect(countSheep(1)).To(Equal("1 sheep..."))
+	})
+})
+
+*/
