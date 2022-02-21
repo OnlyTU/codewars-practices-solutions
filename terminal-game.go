62d8f9e97b7601f4bbdd25e1103ba58edae954c8
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-21 15:39:36 +0300

    8kyu - Grasshopper - Terminal game move function

--- terminal-game.go
+++ terminal-game.go
@@ -1,6 +1,17 @@
 /*
 
-Solution Explanation
+Question:
+
+Terminal game move function
+In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Example:
+move(3, 6) should equal 15
+
+
+Solution Explanation:
 
 - The hero must move 2 times the value of the rolled dice. 
 
@@ -10,5 +21,17 @@ Solution Explanation
 package kata
 
 func Move(position int, roll int) int {
-    return position + 2*roll
+    return position + 2 * roll
 }
+
+/*
+
+Test:
+
+var _ = Describe("Basic Tests", func() {
+    It("Move(0, 4)", func() { Expect(Move(0, 4)).To(Equal(8)) })
+    It("Move(3, 6)", func() { Expect(Move(3, 6)).To(Equal(15)) })
+    It("Move(2, 5)", func() { Expect(Move(2, 5)).To(Equal(12)) })
+})
+
+*/
