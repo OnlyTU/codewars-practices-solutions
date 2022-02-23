a7b45ac23a6a47106aa8338a4204d3cf874bba15
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 11:57:46 +0300

    8kyu -  Find the smallest integer in the array

--- smallest-element.go
+++ smallest-element.go
@@ -31,15 +31,4 @@ func SmallestIntegerFinder(numbers []int) int {
   return min 
 }
 
-/*
 
-Test:
-
-var _ = Describe("Sample Test Cases", func() {
-  It("should work for sample tests", func() {
-    Expect(Expect(SmallestIntegerFinder([]int{34, 15, 88, 2})).To(Equal(2)))
-    Expect(Expect(SmallestIntegerFinder([]int{34, -345, -1, 100})).To(Equal(-345)))
-  })
-})
-
-*/
