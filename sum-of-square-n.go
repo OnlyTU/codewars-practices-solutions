1cac173a897018a29752d632ac218c6462ddb840
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 12:00:17 +0300

    8kyu - Square(n) Sum

--- sum-of-square-n.go
+++ sum-of-square-n.go
@@ -26,14 +26,4 @@ func SquareSum(numbers []int) int {
   return result
 }
 
- /*
  
-Test:
-
-var _ = Describe("Sample Tests",func() {
-    It("Testing [1,2]",func() {Expect(SquareSum([]int{1,2})).To(Equal(5))})
-    It("Testing [0,3,4,5]",func() {Expect(SquareSum([]int{0,3,4,5})).To(Equal(50))})
-    It("Testing []",func() {Expect(SquareSum([]int{})).To(Equal(0))})
-})
-
-*/
