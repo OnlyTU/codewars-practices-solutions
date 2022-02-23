76482951b52cb6459df222ecaef4b3d68f04f590
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 11:38:18 +0300

    8kyu - If you can't sleep, just count sheep!!

--- count-sheep.go
+++ count-sheep.go
@@ -33,16 +33,4 @@ func countSheep(num int) string {
   return temp 
 }
 
-/*
 
-Test:
-
-var _ = Describe("Sample Test Cases", func() {
-	It("The solution should return the correct values for the sample test cases!", func() {
-		Expect(countSheep(2)).To(Equal("1 sheep...2 sheep..."))
-		Expect(countSheep(0)).To(Equal(""))
-		Expect(countSheep(1)).To(Equal("1 sheep..."))
-	})
-})
-
-*/
