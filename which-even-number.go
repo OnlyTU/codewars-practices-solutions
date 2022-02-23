bc3dcfe168f311096f61074cca51432f9d8b6546
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 12:08:04 +0300

    8kyu - Get Nth Even Number

--- which-even-number.go
+++ which-even-number.go
@@ -27,16 +27,4 @@ func NthEven(n int) int {
   return 2 * n - 2
 }
 
-/*
 
-Test:
-
-var _ = Describe("Basic Tests", func() {
-    It("Testing for 1", func() { Expect(NthEven(1)).To(Equal(0)) })
-    It("Testing for 2", func() { Expect(NthEven(2)).To(Equal(2)) })
-    It("Testing for 3", func() { Expect(NthEven(3)).To(Equal(4)) })
-    It("Testing for 100", func() { Expect(NthEven(100)).To(Equal(198)) })
-    It("Testing for 1298734", func() { Expect(NthEven(1298734)).To(Equal(2597466)) })
-})
-
-*/
