cecfd31a852989a573ce831ec33fd5fc5ccdc6fd
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 12:02:38 +0300

    8kyu - Grasshopper - Terminal game move function

--- terminal-game.go
+++ terminal-game.go
@@ -24,14 +24,4 @@ func Move(position int, roll int) int {
     return position + 2 * roll
 }
 
-/*
 
-Test:
-
-var _ = Describe("Basic Tests", func() {
-    It("Move(0, 4)", func() { Expect(Move(0, 4)).To(Equal(8)) })
-    It("Move(3, 6)", func() { Expect(Move(3, 6)).To(Equal(15)) })
-    It("Move(2, 5)", func() { Expect(Move(2, 5)).To(Equal(12)) })
-})
-
-*/
