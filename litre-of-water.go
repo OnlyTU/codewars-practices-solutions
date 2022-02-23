560c4e22a78c9b9fc66d1b79b65b4462f4c5ec76
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 11:47:53 +0300

    8kyu - Keep Hydrated!

--- litre-of-water.go
+++ litre-of-water.go
@@ -21,18 +21,4 @@ func Litres(time float64) int {
   return int(time*0.5)
 }
 
-/*
 
-var _ = Describe("Test Examples", func() {
-	It("Should return the correct values for the sample test cases!", func() {
-		Expect(Litres(2)).To(Equal(1))
-    Expect(Litres(1.4)).To(Equal(0))
-    Expect(Litres(12.3)).To(Equal(6))
-    Expect(Litres(0.82)).To(Equal(0))
-    Expect(Litres(11.8)).To(Equal(5))
-    Expect(Litres(1787)).To(Equal(893))
-    Expect(Litres(0)).To(Equal(0))
-  })
-})
-
-*/
