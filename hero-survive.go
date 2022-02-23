ea926a6e2c334e3752c8e4f890daaaf854d4b5b9
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 11:44:16 +0300

    8kyu -  Is he gonna survive?

--- hero-survive.go
+++ hero-survive.go
@@ -21,19 +21,3 @@ func Hero(bullets, dragons int) bool {
 }
 
 
-/*
-
-- Test
-
-var _ = Describe("Fixed tests", func() {
-   It("should test that the solution returns the correct value", func() {
-     Expect(Hero(10, 5)).To(Equal(true))
-     Expect(Hero(7, 4)).To(Equal(false))
-     Expect(Hero(4, 5)).To(Equal(false))
-     Expect(Hero(100, 40)).To(Equal(true))
-     Expect(Hero(1500, 751)).To(Equal(false))
-     Expect(Hero(0, 1)).To(Equal(false))
-   })
-})
-
-*/
