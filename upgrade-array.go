ab61d93b608ec8f56cd00035fee8b4640e15f532
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 12:05:10 +0300

    8kyu - Beginner - Lost Without a Map

--- upgrade-array.go
+++ upgrade-array.go
@@ -20,23 +20,11 @@ package kata
 
 func Maps(x []int) []int {
   var s []int
-  for i:=0; i<len(x); i++{
-    temp:=2*x[i]
+  for i := 0; i < len(x); i++ {
+    temp := 2 * x[i]
     s = append(s, temp)
   }
   return s
 }
 
-/*
 
-Test:
-
-var _ = Describe("Example tests", func() {
-  It("should test that the solution returns the correct value", func() {
-    Expect(Maps([]int{1, 2, 3})).To(Equal([]int{2, 4, 6}))
-    Expect(Maps([]int{4, 1, 1, 1, 4})).To(Equal([]int{8, 2, 2, 2, 8}))
-    Expect(Maps([]int{2, 2, 2, 2, 2, 2})).To(Equal([]int{4, 4, 4, 4, 4, 4}))
-  })
-})
-
-*/
