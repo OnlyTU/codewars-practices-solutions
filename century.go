609f64132288a56991d287c2b339d15cc22b2871
Tugberk Uguzlar <59655833+OnlyTU@users.noreply.github.com>
2022-02-23 11:31:57 +0300

    8kyu - Century From Year (update)

--- century.go
+++ century.go
@@ -39,17 +39,3 @@ func century(year int) int {
   return int (year / 100)
 }
 
-/*
-
-var _ = Describe("Basic tests", func() {
-	It("should return the correct values", func() {
-		Expect(century(int(1990))).To(Equal(20))
-		Expect(century(int(1705))).To(Equal(18))
-		Expect(century(int(1900))).To(Equal(19))
-		Expect(century(int(1601))).To(Equal(17))
-		Expect(century(int(2000))).To(Equal(20))
-		Expect(century(int(89))).To(Equal(1))
-	})
-})
-
-*/
